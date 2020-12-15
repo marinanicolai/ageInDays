130acc9d437980f839e73fba03f85d07d73b97b7
Marina Nicolai <nicolaimarina@gmail.com>
2020-12-15 14:31:43 -0500

    ageInDays function

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+function ageInDays() {
+  var birthYear = prompt("What year were you born?");
+  var ageInDays = (2020 - birthYear) * 365;
+  var h1 = document.createElement("h1");
+  var textAnswer = document.createTextNode(
+    "You are " + ageInDays + " days old"
+  );
+  h1.setAttribute("id", "ageInDays");
+  h1.appendChild(textAnswer);
+  document.getElementById("flex-box-result").appendChild(h1);
+}
